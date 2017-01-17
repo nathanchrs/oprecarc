7a57e095e762b584c4185b76ff004a9c39807fcf
Jonathan Christopher <jonathan.christopher@outlook.com>
2017-01-17 17:23:39 +0700

    Added user registration

--- app/routes/users.js
+++ app/routes/users.js
@@ -6,6 +6,7 @@ var passport = require('../components/passport.js');
 var winston = require('../components/winston.js');
 var knex = require('../components/knex.js');
 var validation = require('../components/validation.js');
+var bcrypt = require('bcryptjs');
 
 var userSchema = require('../schemas/user.js');
 
@@ -63,6 +64,7 @@ router.get('/users/:nim([0-9]{8})', auth.isLoggedIn, function (req, res, next) {
     .where('nim', req.params.nim)
     .from('users')
     .then(function (user) {
+      if (!user) return res.sendStatus(404);
       return res.render('user', { user: user });
     })
     .catch(function (err) {
@@ -70,19 +72,66 @@ router.get('/users/:nim([0-9]{8})', auth.isLoggedIn, function (req, res, next) {
     }); 
 });
 
-router.get('/users/create', auth.role('admin'), function (req, res) {
+// Note: add auth.isLoggedIn middleware here to prevent non-registerd users form registering
+router.get('/users/create', function (req, res) {
+  if (req.user && req.user.role !== 'admin') return res.redirect('/'); // If not logged in yet, still can create user
   res.render('user-edit');
 });
 
-router.post('/users', auth.role('admin'), function (req, res, next) {
-  // TODO: create register form and handler
-  // hash password using bcryptjs: bcrypt.hash('password', 8, function(err, hash) { });
+// Note: add auth.isLoggedIn middleware here to prevent non-registerd users form registering
+router.post('/users', function (req, res, next) {
+  if (req.user && req.user.role !== 'admin') return res.redirect('/'); // If not logged in yet, still can create user
+
+  var insertSchema = (req.user && req.user.role === 'admin') ? userSchema.userInsertAdminSchema : userSchema.userInsertSchema;
+  var input = validation.validate(req.body, insertSchema);
+  if (input.error) return validation.errorRedirect(input.error, '/users/create', req, res);
+
+  knex.select('nim').from('users').where('nim', input.value.nim).first()
+    .then(function (nim) {
+      if (nim) return validation.errorRedirect({ details: [{ message: 'NIM already exists.', path: 'nim' }], _object: req.body }, '/users/create', req, res);
+
+      bcrypt.hash(input.value.password, 8) // 8 is the salt length to generate
+        .then(function (hash) {
+          var insertData = {
+            nim: input.value.nim,
+            name: input.value.name,
+            gender: input.value.gender,
+            email: input.value.email,
+            phone: input.value.phone,
+            line: input.value.line,
+            password: hash,
+            bio: input.value.bio,
+            created_at: new Date(),
+            updated_at: new Date()
+          };
+          insertData.role = (req.user && req.user.role === 'admin') ? input.value.role : 'cakru';
+
+          return knex('users').insert(insertData);
+        })
+        .then(function () {
+          if (req.user) {
+            winston.log('verbose', 'User with NIM ' + input.value.nim + ' created by ' + req.user.nim + '.');
+            return res.redirect('/users/' + input.value.nim);
+          } else {
+            winston.log('verbose', 'User with NIM ' + input.value.nim + ' registered.');
+            req.flash('info', 'Registration successful, you can now log in.');
+            return res.redirect('/login');
+          }
+        })
+        .catch(function (err) {
+          return next(err);
+        }); 
+    })
+    .catch(function (err) {
+      return next(err);
+    });
 });
 
 router.get('/users/:nim([0-9]{8})/edit', auth.isNimOwnerOrAdmin, function (req, res, next) {
   knex.first('nim', 'name', 'gender', 'email', 'phone', 'line', 'bio', 'role', 'created_at', 'updated_at')
   .where('nim', req.params.nim).from('users')
     .then(function (user) {
+      if (!user) return res.sendStatus(404);
       return res.render('user-edit', { user: user });
     })
     .catch(function (err) {
--- app/schemas/user.js
+++ app/schemas/user.js
@@ -20,7 +20,31 @@ module.exports = {
     line: Joi.string().default('').allow(''),
     bio: Joi.string().default('').allow(''),
     role: Joi.string().regex(/^(cakru|kru|admin)$/).required()
+  }),
+
+  userInsertSchema: Joi.object().keys({
+    nim: Joi.string().regex(/^[0-9]{8}$/).required(),
+    name: Joi.string().required(),
+    gender: Joi.string().regex(/^(male|female)$/).required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string().required(),
+    line: Joi.string().default('').allow(''),
+    password: Joi.string().required(),
+    bio: Joi.string().default('').allow('')
+  }),
+
+  userInsertAdminSchema: Joi.object().keys({
+    nim: Joi.string().regex(/^[0-9]{8}$/).required(),
+    name: Joi.string().required(),
+    gender: Joi.string().regex(/^(male|female)$/).required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string().required(),
+    line: Joi.string().default('').allow(''),
+    password: Joi.string().required(),
+    bio: Joi.string().default('').allow(''),
+    role: Joi.string().regex(/^(cakru|kru|admin)$/).required()
   })
+
 };
 
 
